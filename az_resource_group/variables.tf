3d372dc471bb34b8d4583190ba2ff6d997645c7c
Lars.Panzerbjrn <lars@panzerbjrn.eu>
2025-06-09 22:51:03 +0200

    Refreshing

--- az_resource_group/variables.tf
+++ az_resource_group/variables.tf
@@ -1,7 +1,19 @@
-variable "resource_group" {
-  type = object({
-    name     = string
-    location = string
-    tags     = map(string)
-  })
+# variable "resource_group" {
+#   type = object({
+#     name     = string
+#     location = string
+#     tags     = map(string)
+#   })
+# }
+
+variable "RG_Map" {
+  type = list(object({
+    name        = string
+    function    = string
+    instance_id = string
+    location    = string
+    tags        = map(string)
+  }))
+  description = "List of Resource Groups with their properties for module usage."
+  default     = []
 }
